c3e46414e2a4d59df818496a5b9cfc98a9905ac7
Temirkhan Zimanov <zimanovtemirkhan@gmail.com>
2020-01-30 18:07:28 +0300

    rewrite bridges

--- graphs/bridges.cpp
+++ graphs/bridges.cpp
@@ -8,50 +8,39 @@ typedef long long ll;
 #define fi first
 #define se second
 
-struct Edge {
-    int to, id;
-};
+const int N = 2e4 + 5;
 
-class Graph {
-public:
-    explicit Graph(int n) : g(n+5), tin(n+5), used(n+5), dp(n+5) {
-    }
-    void add_edge(int u, int v, int id) {
-        g[u].push_back({v, id});
-        g[v].push_back({u, id});
-    }
-    void dfs(int v = 1, int par = 0) {
-        used[v] = 1;
-        tin[v] = dp[v] = timer++;
-        for (Edge e : g[v]) {
-            int u = e.to, id = e.id;
-            if (u == par) continue;
-            if (used[u]) dp[v] = min(dp[v], tin[u]);
-            else {
-                dfs(u, v);
-                dp[v] = min(dp[v], dp[u]);
-                if (dp[u] > tin[v]) bridges.push_back(id);
-            }
-        }
-    }
-    vector<int> find_bridges() {
-        dfs();
-        return bridges;
-    }
+vector<pii> g[N];
+bool used[N];
+int tin[N];
+int dp[N];
+int timer = 0;
+vector<int> bridges;
 
-private:
-    vector<vector<Edge>> g;
-    vector<int> tin, dp;
-    vector<bool> used;
-    int timer = 0;
-    vector<int> bridges;
-};
+void dfs(int v = 1, int par = 0) {
+	used[v] = 1;
+	tin[v] = dp[v] = timer++;
+	for (pii e : g[v]) {
+		int u = e.fi;
+		int id = e.se;
+		if (u == par) continue;
+		if (used[u]) {
+			dp[v] = min(dp[v], tin[u]);
+			continue;
+		}
+		dfs(u, v);
+		dp[v] = min(dp[v], dp[u]);
+		if (dp[u] > tin[v]) {
+			bridges.pb(id);
+		}
+	}
+}
 
 int main() {
     ios_base::sync_with_stdio(0); cin.tie(0);
 #ifdef LOCAL
 	freopen("input.txt", "r", stdin);
 #endif
-    
+	
 	return 0;
 }
