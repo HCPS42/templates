afe8a955fa8e4771aaf7623ca6c3cbebd60147b8
Temirkhan Zimanov <zimanovtemirkhan@gmail.com>
2020-07-09 17:32:33 +0600

    update poly hash

--- strings/poly_hash.cpp
+++ strings/poly_hash.cpp
@@ -1,39 +1,51 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-
-#define pb push_back
-#define ppp pop_back
-#define pii pair<int,int>
-#define fi first
-#define se second
 
 template<int m>
 struct mint {
 	int x;
-	mint(int x = 0) : x(x) {}
-	mint operator-() const { return x ? m - x : 0; }
-	mint operator+(const mint& a) const { int y = x; y += a.x; if (y >= m) y -= m; return y; }
-	mint operator-(const mint& a) const { int y = x; y -= a.x; if (y < 0) y += m; return y; }
-	mint operator*(const mint& a) const { return x * 1ll * a.x % m; }
-	mint& operator++() { if (x + 1 == m) return 0; return x + 1; }
-	mint operator++(int) { mint y(*this); operator++(); return y; }
+	mint(int64_t a = 0) { if (a < 0) a = a % m + m; if (a >= m) a %= m; x = a; }
+	friend istream& operator>>(istream& in, mint& a) { int64_t y; cin >> y; a = y; }
+	friend ostream& operator<<(ostream& out, mint a) { return out << a.x; }
+	explicit operator int() const { return x; }
+	static int mod_inv(int a, int mod = m) {
+		// https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm#Example
+		int g = mod, r = a, z = 0, y = 1;
+		while (r != 0) { int q = g / r; g %= r; swap(g, r); z -= q * y; swap(z, y); }
+		return z < 0 ? z + mod : z;
+	}
+	mint inv() const { return mod_inv(x, m); }
+	friend mint binpow(mint a, int64_t b) { mint res = 1; while (b) { if (b & 1) res *= a; b >>= 1; a *= a; } return res; }
+	mint pow(int64_t b) const { return binpow(*this, b); }
 	mint& operator+=(const mint& a) { x += a.x; if (x >= m) x -= m; return *this; }
 	mint& operator-=(const mint& a) { x -= a.x; if (x < 0) x += m; return *this; }
-	mint& operator*=(const mint& a) { x = x * 1ll * a.x % m; return *this; }
-	bool operator==(const mint& a) { return x == a.x; }
-	bool operator!=(const mint& a) { return x != a.x; }
+	static unsigned fast_mod(uint64_t x, unsigned mod = m) {
+#if !defined(_WIN32) || defined(_WIN64)
+		return x % mod;
+#endif
+		// Optimized mod for Codeforces 32-bit machines.
+		// x must be less than 2^32 * mod for this to work, so that x / mod fits in a 32-bit integer.
+		unsigned x_high = x >> 32, x_low = (unsigned) x; unsigned quot, rem;
+		asm("divl %4\n" : "=a" (quot), "=d" (rem) : "d" (x_high), "a" (x_low), "r" (mod));
+		return rem;
+	}
+	mint& operator*=(const mint& a) { x = fast_mod((uint64_t) x * a.x); return *this; }
+	mint& operator/=(const mint& a) { return *this *= a.inv(); }
+	friend mint operator+(const mint& a, const mint& b) { return mint(a) += b; }
+	friend mint operator-(const mint& a, const mint& b) { return mint(a) -= b; }
+	friend mint operator*(const mint& a, const mint& b) { return mint(a) *= b; }
+	friend mint operator/(const mint& a, const mint& b) { return mint(a) /= mint(b); }
+	mint& operator++() { x = x == m - 1 ? 0 : x + 1; return *this; }
+	mint& operator--() { x = x == 0 ? m - 1 : x - 1; return *this; }
+	mint operator++(int) { mint a = *this; ++*this; return a; }
+	mint operator--(int) { mint a = *this; --*this; return a; }
+	mint operator-() const { return x ? m - x : 0; }
+	bool operator==(const mint& a) const { return x == a.x; }
+	bool operator!=(const mint& a) const { return x != a.x; }
 };
 
 template<int m>
-ostream& operator<<(ostream& out, const mint<m>& a) {
-	return out << a.x;
-}
-
-template<int m>
 struct PolyHash {
-	// mt19937 rnd;
-	// mint<m> x = rnd() % int(sqrt(m)) + 1000;
 	mint<m> x;
 	PolyHash(int x) : x(x) {}
 	vector<mint<m>> pw;
@@ -69,15 +81,6 @@ const int m2 = 985661441;
 PolyHash<m1> h1(518963131);
 PolyHash<m2> h2(379359013);
 
-void Solve() {
-
-}
-
 int main() {
-	ios_base::sync_with_stdio(0); cin.tie(0);
-#ifdef LOCAL
-	freopen("input.txt", "r", stdin);
-#endif
-	Solve();
 	return 0;
 }
