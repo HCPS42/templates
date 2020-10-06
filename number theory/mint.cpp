01d8290937285017dc329dc7298ca9e7a20c0bb2
Temirkhan Zimanov <zimanovtemirkhan@gmail.com>
2020-10-07 04:13:37 +0600

    Update mint.cpp

--- number theory/mint.cpp
+++ number theory/mint.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 template<int m>
 struct mint {
-	int x;
+	int x = 0;
 	mint(int64_t a = 0) { if (a < 0) a = a % m + m; if (a >= m) a %= m; x = a; }
 	friend istream& operator>>(istream& in, mint& a) { int64_t y; cin >> y; a = y; return in; }
 	friend ostream& operator<<(ostream& out, mint a) { return out << a.x; }
@@ -17,6 +17,7 @@ struct mint {
 	mint inv() const { return mod_inv(x, m); }
 	friend mint binpow(mint a, int64_t b) { mint res = 1; while (b) { if (b & 1) res *= a; b >>= 1; a *= a; } return res; }
 	mint pow(int64_t b) const { return binpow(*this, b); }
+	mint operator-() const { return x ? m - x : 0; }
 	mint& operator+=(const mint& a) { x += a.x; if (x >= m) x -= m; return *this; }
 	mint& operator-=(const mint& a) { x -= a.x; if (x < 0) x += m; return *this; }
 	static unsigned fast_mod(uint64_t x, unsigned mod = m) {
@@ -39,7 +40,6 @@ struct mint {
 	mint& operator--() { x = x == 0 ? m - 1 : x - 1; return *this; }
 	mint operator++(int) { mint a = *this; ++*this; return a; }
 	mint operator--(int) { mint a = *this; --*this; return a; }
-	mint operator-() const { return x ? m - x : 0; }
 	bool operator==(const mint& a) const { return x == a.x; }
 	bool operator!=(const mint& a) const { return x != a.x; }
 };
