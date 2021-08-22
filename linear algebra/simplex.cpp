63653d71104bd0a0415dc9d615dc2f76ff88a393
TemirkhanZimanov <33005488+TemirkhanZimanov@users.noreply.github.com>
2021-08-22 18:46:39 +0300

    Update simplex.cpp

--- linear algebra/simplex.cpp
+++ linear algebra/simplex.cpp
@@ -1,41 +1,41 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef vector<double> vec;
+typedef vector<double> Vec;
 const double eps = 1e-8;
 
 // Ax <= b, maximize c^Tx
 // x >= 0
 
-vec simplex(vector<vec> A, vec b, vec c) {
+Vec simplex(vector<Vec> A, Vec b, Vec c) {
     int n = A.size();
     int m = A[0].size() + 1;
     int r = n;
     int s = m - 1;
-    vector<vec> D(n + 2, vec(m + 1));
+    vector<Vec> D(n + 2, Vec(m + 1));
     vector<int> ix(n + m);
-    for (int i=0; i<n+m; i++) ix[i] = i;
-    for (int i=0; i<n; i++) {
-        for (int j=0; j<m-1; j++) D[i][j] = -A[i][j];
-        D[i][m-1] = 1;
+    for (int i = 0; i < n + m; i++) ix[i] = i;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m - 1; j++) D[i][j] = -A[i][j];
+        D[i][m - 1] = 1;
         D[i][m] = b[i];
         if (D[r][m] > D[i][m]) r = i;
     }
-    for (int j=0; j<m-1; j++) D[n][j] = c[j];
-    D[n+1][m-1] = -1;
+    for (int j = 0; j < m - 1; j++) D[n][j] = c[j];
+    D[n + 1][m - 1] = -1;
     double d;
     while (1) {
         if (r < n) {
             swap(ix[s], ix[r+m]);
             D[r][s] = 1.0 / D[r][s];
-            for (int j=0; j<=m; j++) {
+            for (int j = 0; j <= m; j++) {
                 if (j != s) {
                     D[r][j] *= -D[r][s];
                 }
             }
-            for (int i=0; i<=n+1; i++) {
+            for (int i = 0; i <= n + 1; i++) {
                 if (i == r) continue;
-                for (int j=0; j<=m; j++) {
+                for (int j = 0; j <= m; j++) {
                     if (j != s) {
                         D[i][j] += D[r][j] * D[i][s];
                     }
@@ -44,18 +44,18 @@ vec simplex(vector<vec> A, vec b, vec c) {
             }
         }
         r = s = -1;
-        for (int j=0; j<m; j++) {
+        for (int j = 0; j < m; j++) {
             if (s < 0 || ix[s] > ix[j]) {
-                if (D[n+1][j] > eps || D[n+1][j] > -eps && D[n][j] > eps) {
+                if (D[n + 1][j] > eps || D[n + 1][j] > -eps && D[n][j] > eps) {
                     s = j;
                 }
             }
         }
         if (s < 0) break;
-        for (int i=0; i<n; i++) {
+        for (int i = 0; i < n; i++) {
             if (D[i][s] < -eps) {
                 if (r < 0 || (d = D[r][m] / D[r][s] - D[i][m] / D[i][s]) < -eps
-                    || d < eps && ix[r+m] > ix[i+m]) {
+                    || d < eps && ix[r + m] > ix[i + m]) {
                     r = i;
                 }
             }
@@ -64,13 +64,13 @@ vec simplex(vector<vec> A, vec b, vec c) {
             return {}; // unbounded
         }
     }
-    if (D[n+1][m] < -eps) {
+    if (D[n + 1][m] < -eps) {
         return {}; // infeasible
     }
-    vec x(m - 1);
-    for (int i=m; i<n+m; i++) {
+    Vec x(m - 1);
+    for (int i = m; i < n + m; i++) {
         if (ix[i] < m - 1) {
-            x[ix[i]] = D[i-m][m];
+            x[ix[i]] = D[i - m][m];
         }
     }
     return x; // ans: D[n][m]
