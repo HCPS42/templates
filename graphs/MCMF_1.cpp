41f5f60b785a4acab1241e1fae71a59c6783607c
Temirkhan Zimanov <zimanovtemirkhan@gmail.com>
2021-09-25 18:40:56 +0300

    Update MCMF_1.cpp

--- graphs/MCMF_1.cpp
+++ graphs/MCMF_1.cpp
@@ -4,9 +4,12 @@ using namespace std;
 // https://codeforces.com/contest/1510/problem/B
 // https://official.contest.yandex.com/ptz-summer-2020/contest/19423/problems/E3
 
+// supports negative costs: yes
+// supports negative cycles: I don't know
+
 typedef long long ll;
 
-const int N = 2105;
+const int N = 2005;
 const ll inf = 4e18;
 
 struct Edge {
@@ -119,84 +122,6 @@ pair<ll, ll> min_cost_max_flow(int src, int sink, ll bound = inf) {
     return {flow, cost};
 }
 
-int a[N];
-int to[N];
-int from[N];
-vector<int> st;
-vector<int> ans;
-int d;
-
-void go(int x) {
-    for (int i = 0; i < d; i++) {
-        if (x & (1 << i)) {
-            ans.push_back(i);
-        }
-    }
-}
-
 int main() {
-    int n;
-    cin >> d >> n;
-    for (int i = 1; i <= n; i++) {
-        string s;
-        cin >> s;
-        for (int j = 0; j < d; j++) {
-            if (s[j] == '1') {
-                a[i] |= 1 << j;
-            }
-        }
-    }
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= n; j++) {
-            if (i == j) continue;
-            if ((a[i] & a[j]) == a[i]) {
-                add_edge(i, j + n, 1, 0);
-            }
-        }
-    }
-    int src = 0;
-    int sink = 2 * n + 1;
-    for (int i = 1; i <= n; i++) {
-        add_edge(src, i, 1, - __builtin_popcount(a[i]) - 1);
-    }
-    for (int i = n + 1; i <= 2 * n; i++) {
-        add_edge(i, sink, 1, 0);
-    }
-    min_cost_max_flow(src, sink);
-    for (auto e : edges) {
-        int u = e.s;
-        int v = e.e;
-        int flow = e.flow;
-        if (1 <= u && u <= n && n + 1 <= v && v <= 2 * n && flow > 0) {
-            to[u] = v - n;
-            from[v - n] = u;
-        }
-    }
-    for (int i = 1; i <= n; i++) {
-        if (from[i] == 0) {
-            st.push_back(i);
-        }
-    }
-    for (int i = 0; i < st.size(); i++) {
-        int v = st[i];
-        go(a[v]);
-        while (to[v]) {
-            go(a[to[v]] ^ a[v]);
-            v = to[v];
-        }
-        if (i + 1 < st.size()) {
-            ans.push_back(-1);
-        }
-    }
-    cout << ans.size() << "\n";
-    for (int i : ans) {
-        if (i == -1) {
-            cout << "R ";
-        }
-        else {
-            cout << i << " ";
-        }
-    }
-    cout << "\n";
     return 0;
 }
