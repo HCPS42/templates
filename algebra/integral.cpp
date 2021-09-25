4e5abc97d8aca418295ef8e63c2c3f5531231966
Temirkhan Zimanov <zimanovtemirkhan@gmail.com>
2021-09-25 13:57:32 +0300

    Update integral.cpp

--- algebra/integral.cpp
+++ algebra/integral.cpp
@@ -1,27 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const double eps = 1e-10;
+typedef long double Real;
 
-double f(double x) {
+const Real eps = 1e-10;
 
+Real f(Real x) {
+    
 }
 
-double simpson(double a, double b) {
-    return 1.0 / 6.0 * (b - a) * (f(a) + 4 * f((a + b) / 2.0) + f(b));
+Real simpson(Real a, Real b) {
+    return Real(1) / Real(6) * (b - a) * (f(a) + Real(4) * f((a + b) / Real(2)) + f(b));
 }
 
-double integral(double a, double b) {
-    double mid  = (a + b) / 2.0;
-    double a1 = simpson(a, b);
-    double b1 = simpson(a, mid) + simpson(mid, b);
-    if (abs(a1 - b1) < eps) return b1;
-    return integral(a, mid) + integral(mid, b);
+Real integral(Real l, Real r) {
+    Real m  = (l + r) / Real(2);
+    Real tot = simpson(l, r);
+    Real sum = simpson(l, m) + simpson(m, r);
+    if (abs(tot - sum) < eps) return sum;
+    return integral(l, m) + integral(m, r);
 }
 
 int main() {
     mt19937 rnd;
-    uniform_real_distribution<double> dist(0.0, 1.0);
-    double num = dist(rnd);
+    uniform_real_distribution<Real> dist(0.0, 1.0);
+    Real num = dist(rnd);
+    cout << num << endl;
     return 0;
 }
