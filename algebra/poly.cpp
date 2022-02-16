3ea0de54a9d02c349fd2ed65ce307adcc64fcc93
Temirkhan Zimanov <zimanovtemirkhan@gmail.com>
2022-02-17 00:02:22 +0300

    Update poly.cpp

--- algebra/poly.cpp
+++ algebra/poly.cpp
@@ -49,84 +49,84 @@ struct mint {
 const int mod = 998244353; // 985661441
 
 namespace NTT {
-	const int K = 1 << 20; // be careful
-	const mint<mod> root = binpow(mint<mod>(3), (mod - 1) / K);
-	// 3 is a primitive root modulo mod
+    const int K = 1 << 20; // be careful
+    const mint<mod> root = binpow(mint<mod>(3), (mod - 1) / K);
+    // 3 is a primitive root modulo mod
 
-	typedef vector<mint<mod>> poly;
+    typedef vector<mint<mod>> poly;
 
-	mint<mod> prec_w[K / 2];
-	mint<mod> w[K];
+    mint<mod> prec_w[K / 2];
+    mint<mod> w[K];
 
-	bool initialized = 0;
-	void init() {
-		if (initialized) {
-			return;
-		}
-		initialized = 1;
-		prec_w[0] = 1;
-		for (int i = 1; i < K / 2; i++) {
-			prec_w[i] = prec_w[i - 1] * root;
-		}
-		for (int i = 1; i < K; i *= 2) {
-			for (int j = 0; j < i; j++) {
-				w[i + j] = prec_w[K / (2 * i) * j];
-			}
-		}
-	}
+    bool initialized = 0;
+    void init() {
+        if (initialized) {
+            return;
+        }
+        initialized = 1;
+        prec_w[0] = 1;
+        for (int i = 1; i < K / 2; i++) {
+            prec_w[i] = prec_w[i - 1] * root;
+        }
+        for (int i = 1; i < K; i *= 2) {
+            for (int j = 0; j < i; j++) {
+                w[i + j] = prec_w[K / (2 * i) * j];
+            }
+        }
+    }
 
-	void fft(mint<mod>* in, mint<mod>* out, int n, int k = 1) {
-		if (n == 1) {
-			*out = *in;
-			return;
-		}
-		n /= 2;
-		fft(in, out, n, 2 * k);
-		fft(in + k, out + n, n, 2 * k);
-		for (int i = 0; i < n; i++) {
-			mint<mod> t = out[i + n] * w[i + n];
-			out[i + n] = out[i] - t;
-			out[i] += t;
-		}
-	}
+    void fft(mint<mod>* in, mint<mod>* out, int n, int k = 1) {
+        if (n == 1) {
+            *out = *in;
+            return;
+        }
+        n /= 2;
+        fft(in, out, n, 2 * k);
+        fft(in + k, out + n, n, 2 * k);
+        for (int i = 0; i < n; i++) {
+            mint<mod> t = out[i + n] * w[i + n];
+            out[i + n] = out[i] - t;
+            out[i] += t;
+        }
+    }
 
-	void align(poly& a, poly& b) {
-		int n = a.size() + b.size() - 1;
-		while (a.size() < n) a.push_back(0);
-		while (b.size() < n) b.push_back(0);
-	}
+    void align(poly& a, poly& b) {
+        int n = a.size() + b.size() - 1;
+        while (a.size() < n) a.push_back(0);
+        while (b.size() < n) b.push_back(0);
+    }
 
-	poly eval(poly& a) {
-		while (__builtin_popcount(a.size()) != 1) {
-			a.push_back(0);
-		}
-		poly res(a.size());
-		fft(a.data(), res.data(), a.size());
-		return res;
-	}
+    poly eval(poly& a) {
+        while (__builtin_popcount(a.size()) != 1) {
+            a.push_back(0);
+        }
+        poly res(a.size());
+        fft(a.data(), res.data(), a.size());
+        return res;
+    }
 
-	poly inter(poly a) {
-		int n = a.size();
-		poly res(n);
-		fft(a.data(), res.data(), n);
-		for (int i = 0; i < n; i++) {
-			res[i] /= n;
-		}
-		reverse(res.begin() + 1, res.end());
-		return res;
-	}
+    poly inter(poly a) {
+        int n = a.size();
+        poly res(n);
+        fft(a.data(), res.data(), n);
+        for (int i = 0; i < n; i++) {
+            res[i] /= n;
+        }
+        reverse(res.begin() + 1, res.end());
+        return res;
+    }
 
-	poly mult(poly a, poly b) {
-		init();
-		align(a, b);
-		a = eval(a);
-		b = eval(b);
-		for (int i = 0; i < a.size(); i++) {
-			a[i] *= b[i];
-		}
-		a = inter(a);
-		return a;
-	}
+    poly mult(poly a, poly b) {
+        init();
+        align(a, b);
+        a = eval(a);
+        b = eval(b);
+        for (int i = 0; i < a.size(); i++) {
+            a[i] *= b[i];
+        }
+        a = inter(a);
+        return a;
+    }
 }
 
 template <class T>
@@ -134,24 +134,24 @@ vector<T> mult_fft(const vector<T>& a, const vector<T>& b);
 
 template <>
 vector<mint<mod>> mult_fft(const vector<mint<mod>>& a,
-		const vector<mint<mod>>& b) {
-	return NTT::mult(a, b);
+        const vector<mint<mod>>& b) {
+    return NTT::mult(a, b);
 }
 
 template <class T>
 vector<T> mult_slow(const vector<T>& a, const vector<T>& b) {
-	if (a.empty() || b.empty()) {
-		return {};
-	}
-	vector<T> res = a;
-	res.resize(a.size() + b.size() - 1);
-	for (int k = res.size() - 1; k >= 0; k--) {
-		res[k] *= b[0];
-		for (int j = 1; j < min(k + 1, (int) b.size()); j++) {
-			res[k] += res[k - j] * b[j];
-		}
-	}
-	return res;
+    if (a.empty() || b.empty()) {
+        return {};
+    }
+    vector<T> res = a;
+    res.resize(a.size() + b.size() - 1);
+    for (int k = res.size() - 1; k >= 0; k--) {
+        res[k] *= b[0];
+        for (int j = 1; j < min(k + 1, (int) b.size()); j++) {
+            res[k] += res[k - j] * b[j];
+        }
+    }
+    return res;
 }
 
 template <class T>
@@ -172,29 +172,29 @@ struct poly {
     poly(initializer_list<T> b) : a(b) {
         trim();
     }
-	friend ostream& operator<<(ostream& out, poly a) {
-		out << "{";
-		for (int i = 0; i <= a.deg(); i++) {
-			if (i > 0) {
-				out << ", ";
-			}
-			out << a[i];
-		}
-		out << "}";
-		return out;
-	}
+    friend ostream& operator<<(ostream& out, poly a) {
+        out << "{";
+        for (int i = 0; i <= a.deg(); i++) {
+            if (i > 0) {
+                out << ", ";
+            }
+            out << a[i];
+        }
+        out << "}";
+        return out;
+    }
     int deg() const {
         if (a.empty()) {
             return -1;
         }
         return a.size() - 1;
     }
-	T& operator[](size_t i) {
-		if (i < 0 || deg() < i) {
+    T& operator[](size_t i) {
+        if (i < 0 || deg() < i) {
             assert(0);
         }
         return a[i];
-	}
+    }
     T operator[](size_t i) const {
         if (i < 0 || deg() < i) {
             return T(0);
@@ -210,13 +210,13 @@ struct poly {
         }
         return res;
     }
-	poly operator-() const {
-		poly res = *this;
-		for (int i = 0; i <= deg(); i++) {
-			res[i] = -res[i];
-		}
-		return res;
-	}
+    poly operator-() const {
+        poly res = *this;
+        for (int i = 0; i <= deg(); i++) {
+            res[i] = -res[i];
+        }
+        return res;
+    }
     poly& operator+=(const poly& b) {
         while (deg() < b.deg()) {
             a.push_back(T(0));
@@ -238,14 +238,14 @@ struct poly {
         return *this;
     }
     poly& operator*=(const poly& b) {
-		if (deg() == -1 || b.deg() == -1) {
-			a.clear();
-			return *this;
-		}
-		// a = mult_slow(a, b.a);
-		a = mult_fft(a, b.a);
-		trim();
-		return *this;
+        if (deg() == -1 || b.deg() == -1) {
+            a.clear();
+            return *this;
+        }
+        // a = mult_slow(a, b.a);
+        a = mult_fft(a, b.a);
+        trim();
+        return *this;
     }
     friend poly operator+(poly a, const poly& b) { return a += b; }
     friend poly operator-(poly a, const poly& b) { return a -= b; }
@@ -264,32 +264,32 @@ struct poly {
         }
         return res;
     }
-	poly mod_xk(int k) const { // get first k coefficients
-		return vector<T>(a.begin(), a.begin() + min(k, (int) a.size()));
-	}
-	poly mul_xk(int k) const { // multiply by x^k
-		vector<T> res = a;
-		res.insert(res.begin(), k, 0);
-		return res;
-	}
-	poly div_xk(int k) const { // drop first k coefficients
-		return vector<T>(a.begin() + min(k, (int) a.size()), a.end());
-	}
-	poly substr(int l, int r) const { // return mod_xk(r).div_xk(l)
-		return vector<T>(a.begin() + min(l, (int) a.size()),
-			a.begin() + min(r, (int) a.size()));
-	}
-	poly inv(int n) const {
-		assert((*this)[0] != T(0));
-		poly res{T(1) / a[0]};
-		int k = 1;
-		while (k < n) {
-			poly b = (res * mod_xk(2 * k)).substr(k, 2 * k);
-			res -= (res * b).mod_xk(k).mul_xk(k);
-			k *= 2;
-		}
-		return res.mod_xk(n);
-	}
+    poly mod_xk(int k) const { // get first k coefficients
+        return vector<T>(a.begin(), a.begin() + min(k, (int) a.size()));
+    }
+    poly mul_xk(int k) const { // multiply by x^k
+        vector<T> res = a;
+        res.insert(res.begin(), k, 0);
+        return res;
+    }
+    poly div_xk(int k) const { // drop first k coefficients
+        return vector<T>(a.begin() + min(k, (int) a.size()), a.end());
+    }
+    poly substr(int l, int r) const { // return mod_xk(r).div_xk(l)
+        return vector<T>(a.begin() + min(l, (int) a.size()),
+            a.begin() + min(r, (int) a.size()));
+    }
+    poly inv(int n) const {
+        assert((*this)[0] != T(0));
+        poly res{T(1) / a[0]};
+        int k = 1;
+        while (k < n) {
+            poly b = (res * mod_xk(2 * k)).substr(k, 2 * k);
+            res -= (res * b).mod_xk(k).mul_xk(k);
+            k *= 2;
+        }
+        return res.mod_xk(n);
+    }
 };
 
 int main() {
