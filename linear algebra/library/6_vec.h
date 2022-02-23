665d6d1cbad90ffd6baa160dd2cb4e297bf3e998
Temirkhan Zimanov <zimanovtemirkhan@gmail.com>
2022-02-23 21:11:18 +0300

    Create 6_vec.h

---
+++ linear algebra/library/6_vec.h
@@ -0,0 +1,69 @@
+#pragma once
+
+#include <bits/stdc++.h>
+using namespace std;
+
+template <class T>
+struct Vec {
+    vector<T> a;
+    Vec() = default;
+    Vec(int n) { a.resize(n, 0); }
+    Vec(const vector<T>& b) : a(b) {}
+    friend ostream& operator<<(ostream& out, const Vec& b) {
+        out << "(";
+        for (int i = 0; i < b.size(); i++) {
+            if (i > 0) out << ", ";
+            out << b[i];
+        }
+        out << ")";
+        return out;
+    }
+    int size() const { return a.size(); }
+    T& operator[](int i) { return a[i]; }
+    const T& operator[](int i) const { return a[i]; }
+    Vec& operator+=(const Vec& b) {
+        for (int i = 0; i < size(); i++) {
+            a[i] += b[i];
+        }
+        return *this;
+    }
+    Vec& operator-=(const Vec& b) {
+        for (int i = 0; i < size(); i++) {
+            a[i] -= b[i];
+        }
+        return *this;
+    }
+    Vec& operator*=(const T& b) {
+        for (int i = 0; i < size(); i++) {
+            a[i] *= b;
+        }
+        return *this;
+    }
+    Vec& operator/=(const T& b) {
+        for (int i = 0; i < size(); i++) {
+            a[i] /= b;
+        }
+        return *this;
+    }
+    friend Vec operator+(Vec a, const Vec& b) { return a += b; }
+    friend Vec operator-(Vec a, const Vec& b) { return a -= b; }
+    friend Vec operator*(Vec a, const T& b) { return a *= b; }
+    friend Vec operator*(const T& a, Vec b) { return b *= a; }
+    friend Vec operator/(Vec a, const T& b) { return a /= b; }
+    friend bool operator==(const Vec& a, const Vec& b) { return a.a == b.a; }
+    friend T operator*(const Vec& a, const Vec& b) {
+        T res = 0;
+        for (int i = 0; i < a.size(); i++) {
+            res += a[i] * b[i];
+        }
+        return res;
+    }
+};
+
+template <class T>
+ostream& operator<<(ostream& out, const vector<Vec<T>>& a) {
+    for (const Vec<T>& i : a) {
+        out << i << "\n";
+    }
+    return out;
+}
