6f1715b71cfd6981c912a3bef663a0ff6d13ee81
HCPS42 <33005488+HCPS42@users.noreply.github.com>
2022-04-10 22:07:15 +0300

    Update CRT.cpp

--- number theory/CRT.cpp
+++ number theory/CRT.cpp
@@ -29,10 +29,12 @@ pair<T, T> crt(T ra, T a, T rb, T b) {
     if (ra % d != rb % d) {
         return {0, 0};
     }
-    T mod = (a / d) * (b / d);
+    a /= d;
+    b /= d;
+    T mod = a * b;
     T r = 0;
-    r = (r + ((ra / d) * (b / d) % mod) * mod_inv(b / d, a / d) % mod) % mod;
-    r = (r + ((rb / d) * (a / d) % mod) * mod_inv(a / d, b / d) % mod) % mod;
+    r = (r + ((ra / d) * b % mod) * mod_inv(b, a) % mod) % mod;
+    r = (r + ((rb / d) * a % mod) * mod_inv(a, b) % mod) % mod;
     mod *= d;
     T res = (r * d % mod + ra % d) % mod;
     return {res, mod};
@@ -46,8 +48,8 @@ pair<T, T> CRT(vector<T> r, vector<T> a) {
     }
     pair<T, T> res = {0, 1};
     for (int i = 0; i < n; i++) {
-        res = crt(res.fi, res.se, r[i], a[i]);
-        if (res.se == 0) {
+        res = crt(res.first, res.second, r[i], a[i]);
+        if (res.second == 0) {
             return {0, 0};
         }
     }
